6d711886309ded13b361e8b97e13e29e5c0b7c93
faizan furqan ahmad <alamfaizan173@gmail.com>
2025-06-04 17:34:49 +0530

    add employees api

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import ForeignKey, CASCADE
+
 
 # Create your models here.
 
@@ -14,3 +16,25 @@ class Company(models.Model):
 
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    position = models.CharField(max_length=100, choices=(
+                                                        ('Manager', 'Manager'),
+                                                        ('Software Developer', 'SD'),
+                                                        ('Project Leader', 'PL')
+                                                        )
+    )
+
+    company = models.ForeignKey(Company, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
--- api/views.py
+++ api/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Company
-from .serializers import CompanySerializer
+from rest_framework import viewsets, decorators, response
+from .models import Company, Employee
+from .serializers import CompanySerializer, EmployeeSerializer
 
 # Create your views here.
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+    @decorators.action(detail=True, methods=['get'])
+    def employees(self, request, pk=None):
+        company = Company.objects.get(pk=pk)
+
+        emps = Employee.objects.filter(company = company)
+
+        emps_serializer = EmployeeSerializer(emps, many=True, context= {'request': request})
+
+        return response.Response(emps_serializer.data)
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset =  Employee.objects.all()
+    serializer_class = EmployeeSerializer
